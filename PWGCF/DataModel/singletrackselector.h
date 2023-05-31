2f7975b854de5aefad636d1be3c4fdacd078a55d
Sofia Tomassini <sofia.tomassini2@studio.unibo.it>
2023-05-31 16:37:16 +0200

    sofia update

--- PWGCF/DataModel/singletrackselector.h
+++ PWGCF/DataModel/singletrackselector.h
@@ -1,23 +1,41 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//
+/// \brief  create a table for single track selection.
+/// \author Sofia Tomassini
+/// \since 30 May 2023
+
+
 #ifndef COMMON_DATAMODEL_SINGLETRACKSELECTOR_H_
 #define COMMON_DATAMODEL_SINGLETRACKSELECTOR_H_
 
 #include "Framework/ASoA.h"
 #include "Framework/AnalysisDataModel.h"
 
-namespace o2::aod::singletrackselector {
-    DECLARE_SOA_INDEX_COLUMN(Collision, collision); // Index to the collision
-    DECLARE_SOA_COLUMN(Px, px, float); // Momentum of the track
-    DECLARE_SOA_COLUMN(Py, py, float); // Momentum of the track
-    DECLARE_SOA_COLUMN(Pz, pz, float); // Momentum of the track
-}
+
+namespace o2::aod
+{
+namespace singletrackselector
+{
+DECLARE_SOA_INDEX_COLUMN(Collision, collision); // Index to the collision
+DECLARE_SOA_COLUMN(Px, px, float);              // Momentum of the track
+DECLARE_SOA_COLUMN(Py, py, float);              // Momentum of the track
+DECLARE_SOA_COLUMN(Pz, pz, float);              // Momentum of the track
+} // namespace singletrackselector
 
 DECLARE_SOA_TABLE(SingleTrackSel, "AOD", "STSEL", // Table of the delta TOF data format. One entry per track couple.
-    o2::soa::Index<>,
-    singletrackselector::CollisionId,
-    singletrackselector::Px,
-    singletrackselector::Py,
-    singletrackselector::Pz);
-
-#endif  
-
-
+                  o2::soa::Index<>,
+                  singletrackselector::CollisionId,
+                  singletrackselector::Px,
+                  singletrackselector::Py,
+                  singletrackselector::Pz);
+} // namespace o2::aod
+#endif
--- PWGCF/TableProducer/singleTrackSelector.cxx
+++ PWGCF/TableProducer/singleTrackSelector.cxx
@@ -1,3 +1,18 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+//
+/// \brief create a table applying some basic cuts on the ITS and DCA.
+/// \author Sofia Tomassini
+/// \since 31 May 2023
+
 #include "Framework/AnalysisTask.h"
 #include "Framework/runDataProcessing.h"
 #include "Common/DataModel/PIDResponse.h"
@@ -11,27 +26,28 @@
 using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
+using namespace o2::track;
 using namespace o2::aod::singletrackselector; // the namespace defined in .h
 
 struct singleTrackSelector {
 
   Configurable<int> applyEvSel{"applyEvSel", 2, "Flag to apply rapidity cut: 0 -> no event selection, 1 -> Run 2 event selection, 2 -> Run 3 event selection"};
-  Configurable<int> trackSelection{"trackSelection", 1, "Track selection: 0 -> No Cut, 1 -> kGlobalTrack, 2 -> kGlobalTrackWoPtEta, 3 -> kGlobalTrackWoDCA, 4 -> kQualityTracks, 5 -> kInAcceptanceTracks"};
+  // Configurable<int> trackSelection{"trackSelection", 1, "Track selection: 0 -> No Cut, 1 -> kGlobalTrack, 2 -> kGlobalTrackWoPtEta, 3 -> kGlobalTrackWoDCA, 4 -> kQualityTracks, 5 -> kInAcceptanceTracks"};
 
-  using Trks = soa::Join<aod::Tracks, aod::TracksExtra,
-                         aod::TOFEvTime, aod::EvTimeTOFOnly, aod::TOFSignal, aod::pidEvTimeFlags,
+  using Trks = soa::Join<aod::Tracks, aod::TracksExtra, aod::pidEvTimeFlags,
                          aod::pidTPCFullEl, aod::pidTPCFullPi, aod::pidTPCFullKa, aod::pidTPCFullPr,
                          aod::pidTOFFullEl, aod::pidTOFFullMu, aod::pidTOFFullPi, aod::pidTOFFullKa, aod::pidTOFFullPr,
                          aod::TrackSelection>;
   using Coll = soa::Join<aod::Collisions, aod::Mults, aod::EvSels, aod::FT0sCorrected>;
 
-  Produces<o2::aod::selectedtracks> tableRow;
+  Produces<o2::aod::SingleTrackSel> tableRow;
 
   Filter eventFilter = (applyEvSel.node() == 0) ||
-                       ((applyEvSel.node() == 1) && (o2::aod::evsel::sel7 == true)) ||
-                       ((applyEvSel.node() == 2) && (o2::aod::evsel::sel8 == true));
-  Filter vertexFilter = (o2::aod::‌collision::PosZ < 15.f && o2::aod::‌collision::PosZ > -15.f);
-  Filter trackFilter = (o2::aod::track::ITSChi2NCl >= 0.f && o2::aod::track::ITSChi2NCl <= 36.f && o2::aod::track::TPCChi2NCl >= 0.f && o2::aod::track::TPCChi2NCl <= 4.f);
+                       ((applyEvSel.node() == 1) && (aod::evsel::sel7 == true)) ||
+                       ((applyEvSel.node() == 2) && (aod::evsel::sel8 == true));
+   Filter vertexFilter = ((o2::aod::collision::posZ < 15.f) && (o2::aod::collision::posZ > -15.f));
+   Filter trackFilter = ((o2::aod::track::itsChi2NCl <= 36.f) && (o2::aod::track::itsChi2NCl >= 0.f) && (o2::aod::track::tpcChi2NCl >= 0.f) && (o2::aod::track::tpcChi2NCl <= 4.f));
+  
 
   void process(soa::Filtered<Coll>::iterator const& collision, soa::Filtered<Trks> const& tracks)
   {
